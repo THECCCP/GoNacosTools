d34447b4cb82656d908dc272b96c833ea23bc127
THECCCP <4nyw4y93@gmail.com>
2023-08-28 15:49:29 +0800

    change main.go

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-	url := flag.String("u", "", "使用 -u 指定URL")
+	url := flag.String("u", "http://127.0.0.1:8848", "使用 -u 指定URL")
 	method := flag.String("m", "", "使用 -m 指定方法")
 	flag.Parse()
 	urls := lib.FormatURL(*url)
 	res := lib.GetNacosUrl(urls)
 	switch *method {
 	case "":
-		fmt.Println("请使用 -m 参数指定方法\n-m getuser 获取用户名密码")
+		fmt.Println("请使用 -m 参数指定方法\n-m getuser 获取用户名密码\n-m adduser 添加用户")
 	case "getuser":
 		lib.GetNacosUser(res)
 	case "adduser":
